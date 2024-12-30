1a266566a0b753c4f22f164d27ca36c7e82b0af2
Maheeza12 <maheezadilthar1998@gmail.com>
2024-12-30 21:59:29 +0530

    localhost updated

--- backend/main.js
+++ backend/main.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'mysql',
+    host: 'localhost',
     user: 'root',
     password: 'MDIsql1223@#',
     database: 'student_management'
