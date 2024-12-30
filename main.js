fc13e7832799f4043161658db0545c7f70779cd9
Maheeza12 <maheezadilthar1998@gmail.com>
2024-12-30 10:58:38 +0530

    Recovered lost files

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+app.use(cors());
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'MDIsql1223@#',
+    database: 'student_management'
 });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.use(express.json());
+
+// Root route (to fix "Cannot GET /")
+app.get('/', (req, res) => {
+    res.send('Welcome to the Student Management System!');
 });
+
+// Fetch all students
+app.get('/students', (req, res) => {
+    db.query('SELECT * FROM students', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Add a student
+app.post('/students', (req, res) => {
+    const { IndexNo, Name, DoB, GPA } = req.body;
+    db.query('INSERT INTO students SET ?', { IndexNo, Name, DoB, GPA }, (err, results) => {
+        if (err) throw err;
+        res.json({ id: results.insertId });
+    });
+});
+
+// Delete a student by ID
+app.delete('/students/:id', (req, res) => {
+    const studentId = req.params.id;
+    db.query('DELETE FROM students WHERE id = ?', [studentId], (err, results) => {
+        if (err) throw err;
+        res.json({ message: 'Student deleted successfully' });
+    });
+});
+
+
+// Start server
+app.listen(3000, () => console.log('Server started on port 3000'));
